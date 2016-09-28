4ebf393454f9d41769a93c62a24753f9917ccb1e
Kit Freddura <kitfreddura@berkeley.edu>
2016-09-27 20:53:27 -0700

    updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 #![allow(dead_code, unused)]
-
 #[macro_use]
 pub mod lift;
-#[macro_use]
 pub mod monad;
-#[macro_use]
 pub mod functor;
-
-#[macro_use]
 pub mod monoid;
-#[macro_use]
 pub mod semigroup;
 pub mod applicative;
-#[macro_use]
 pub mod foldable;
